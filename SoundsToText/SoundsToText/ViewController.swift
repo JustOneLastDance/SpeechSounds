9fd7cd7803340739e37b692301167f34b88f9735
JustinChou <justinchou186@gmail.com>
2016-10-25 12:37:22 +0800

    init the project

---
+++ SoundsToText/SoundsToText/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SoundsToText
+//
+//  Created by JustinChou on 16/10/25.
+//  Copyright © 2016年 JustinChou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
